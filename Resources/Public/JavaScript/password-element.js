4188021a6c02681d0757b21ac66aa232a4a604a5
Georg Ringer <georg.ringer@gmail.com>
2025-06-25 22:22:19 +0200

    improve password meter

--- Resources/Public/JavaScript/password-element.js
+++ Resources/Public/JavaScript/password-element.js
@@ -10,51 +10,93 @@
  *
  * The TYPO3 project - inspiring people to share!
  */
-import a from "@typo3/core/document-service.js";
-import {selector as o} from "@typo3/core/literals.js";
-import d from "@typo3/core/ajax/ajax-request.js";
+import DocumentService from "@typo3/core/document-service.js";
+import { selector } from "@typo3/core/literals.js";
+import AjaxRequest from "@typo3/core/ajax/ajax-request.js";
 import "@typo3/core/ajax/ajax-response.js";
-import t from "@typo3/backend/icons.js";
+import Icons from "@typo3/backend/icons.js";
 
-class m extends HTMLElement {
+class PasswordElement extends HTMLElement {
   constructor() {
-    super(...arguments), this.element = null, this.passwordPolicyInfo = null, this.passwordPolicySet = !1
+    super();
+    this.element = null;
+    this.passwordPolicyInfo = null;
+    this.passwordPolicySet = false;
   }
 
   async connectedCallback() {
-    if (this.element !== null) return;
-    const s = this.getAttribute("recordFieldId");
-    s !== null && (await a.ready(), this.element = this.querySelector(o`#${s}`), this.element && (this.passwordPolicyInfo = this.querySelector(o`#password-policy-info-${this.element.id}`), this.passwordPolicySet = (this.getAttribute("passwordPolicy") || "") !== "", this.registerEventHandler()))
+    if (this.element !== null) {
+      return;
+    }
+
+    const recordFieldId = this.getAttribute("recordFieldId");
+    if (recordFieldId !== null) {
+      await DocumentService.ready();
+      this.element = this.querySelector(selector`#${recordFieldId}`);
+      if (this.element) {
+        this.passwordPolicyInfo = this.querySelector(selector`#password-policy-info-${this.element.id}`);
+        this.passwordPolicySet = (this.getAttribute("passwordPolicy") || "") !== "";
+        this.registerEventHandler();
+      }
+    }
   }
 
   registerEventHandler() {
-    this.passwordPolicySet && this.passwordPolicyInfo !== null && (this.element.addEventListener("focusin", () => {
-      this.passwordPolicyInfo.classList.remove("hidden")
-    }), this.element.addEventListener("focusout", () => {
-      this.passwordPolicyInfo.classList.add("hidden")
-    }), this.element.addEventListener("input", () => {
-      const s = this.querySelectorAll(o`#password-policy-info-${this.element.id} li`),
-        l = Array.from(s).map(i => i.dataset.id);
-      new d(TYPO3.settings.ajaxUrls.password_meter_verify).post({
-        password: this.element.value,
-        list: l
-      }).then(async i => {
-        const n = await i.resolve();
-        n.success === !0 ? s.forEach(e => {
-          e.classList.remove("text-danger"), e.classList.remove("text-success"), e.classList.add("text-success")
-        }) : s.forEach(e => {
-          e.classList.remove("text-danger", "text-success");
-          const c = e.querySelector("span");
-          n.errors.includes(e.getAttribute("data-id")) ? (t.getIcon("actions-exclamation-circle", t.sizes.small).then(r => {
-            c.innerHTML = r
-          }), e.classList.add("text-danger")) : (t.getIcon("actions-check-circle", t.sizes.small).then(r => {
-            c.innerHTML = r
-          }), e.classList.add("text-success"))
-        })
-      }).catch(() => {
-      })
-    }))
+    if (this.passwordPolicySet && this.passwordPolicyInfo !== null) {
+      this.element.addEventListener("focusin", () => {
+        this.passwordPolicyInfo.classList.remove("hidden");
+      });
+
+      this.element.addEventListener("focusout", () => {
+        this.passwordPolicyInfo.classList.add("hidden");
+      });
+
+      this.element.addEventListener("input", () => {
+        const listItems = this.querySelectorAll(selector`#password-policy-info-${this.element.id} li`);
+        const ids = Array.from(listItems).map((li) => li.dataset.id);
+
+        new AjaxRequest(TYPO3.settings.ajaxUrls.password_meter_verify)
+          .post({
+            password: this.element.value,
+            list: ids
+          })
+          .then(async (response) => {
+            const result = await response.resolve();
+            if (result.success === true) {
+              listItems.forEach((li) => {
+                li.classList.remove("text-danger", "text-success");
+                li.classList.add("text-success");
+                const iconTarget = li.querySelector("span");
+                Icons.getIcon("actions-check-circle", Icons.sizes.small).then((icon) => {
+                  iconTarget.innerHTML = icon + '&nbsp;';
+                });
+              });
+            } else {
+              listItems.forEach((li) => {
+                li.classList.remove("text-danger", "text-success");
+
+                const iconTarget = li.querySelector("span");
+
+                if (result.errors.includes(li.getAttribute("data-id"))) {
+                  Icons.getIcon("actions-exclamation-circle", Icons.sizes.small).then((icon) => {
+                    iconTarget.innerHTML = icon + '&nbsp;';
+                  });
+                  li.classList.add("text-danger");
+                } else {
+                  Icons.getIcon("actions-check-circle", Icons.sizes.small).then((icon) => {
+                    iconTarget.innerHTML = icon + '&nbsp;';
+                  });
+                  li.classList.add("text-success");
+                }
+              });
+            }
+          })
+          .catch(() => {
+            // Error handling logic (currently empty)
+          });
+      });
+    }
   }
 }
 
-window.customElements.define("typo3-formengine-element-password", m);
+window.customElements.define("typo3-formengine-element-password", PasswordElement);
